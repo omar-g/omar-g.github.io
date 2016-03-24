9c6821f8fbed345854b07de0752280492002ec56
G Omar <omarr@list.ru>
2016-03-24 22:48:32 +0300

    sewinglab added

---
+++ sewinglab/config.rb
@@ -0,0 +1,27 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "/www/"
+sass_dir = "/develop/"
+images_dir = "/www/assets/img/"
+javascripts_dir = "develop/sass/scripts"
+fonts_dir = "develop/sass/fonts"
+
+output_style = :expanded
+environment = :production
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass develop/sass/sass scss && rm -rf sass && mv scss sass
